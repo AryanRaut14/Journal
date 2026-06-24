420d1e2993e2a684ffe327e7ada629f0eb4b49c2
AryanRaut14 <aryanbraut@gmail.com>
2026-06-24 22:43:39 +0530

    Commit code

---
+++ src/main/java/com/holysecret/journalApp/constants/Placeholders.java
@@ -0,0 +1,6 @@
+package com.holysecret.journalApp.constants;
+
+public interface Placeholders {
+    String API_KEY = "<apiKey>";
+    String CITY = "<city>";
+}
---
+++ src/main/java/com/holysecret/journalApp/enums/Sentiment.java
@@ -0,0 +1,8 @@
+package com.holysecret.journalApp.enums;
+
+public enum Sentiment {
+    HAPPY,
+    SAD,
+    ANGRY,
+    ANXIOUS;
+}
